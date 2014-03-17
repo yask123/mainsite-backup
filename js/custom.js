a71776742c55bf588716bf73f0c240e715bfb4e7
Karan Goel <karan@goel.im>
2014-03-17 07:22:16 -0700

    add some style, and more content

--- js/custom.js
+++ js/custom.js
@@ -14,17 +14,4 @@ $(document).ready(function() {
     }
 
     $('a[href*=#]:not([href=#])').click(scroll);
-
-    $('body').keyup(function(event) {
-        var location = window.pageYOffset;
-        var height = $(window).height();
-        var delta = 3*height/4;
-        var time = 200;
-
-        if (event.keyCode == 39 || event.keyCode == 40) {
-            $('html, body').animate({scrollTop: location+delta}, time);
-        } else if (event.keyCode == 37 || event.keyCode == 38) {
-            $('html, body').animate({scrollTop: location-delta}, time);
-        } 
-    });
 });
