8d9201bff6435e0b77c21f4fd21abd3856409c6f
Karan Goel <karan@goel.io>
2015-04-29 10:35:04 -0700

    remove redundant background image code

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
         reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
       },
 
-    // when this task is run, lint the Gruntfile and all js files in src
+      // when this task is run, lint the Gruntfile and all js files in src
       build: ['Grunfile.js', 'js/custom.js']
     },
 
